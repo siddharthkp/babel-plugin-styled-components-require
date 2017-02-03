8038540bbf121411279e3b0fb78c5cd282b93d7c
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-02-03 23:45:06 +0530

    tiny refactor

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-export default function ({ types: t }) {
+'use strict';
+
+exports.__esModule = true;
+
+exports.default = ({types: t}) => {
   return {
     visitor: {
-      TaggedTemplateExpression(path, { file }) {
-        if (path && path.scope && path.scope.globals && path.scope.globals.styled) file.set('hasStyled', true);
+      TaggedTemplateExpression: (path, {file}) => {
+        if (path.scope && path.scope.globals && path.scope.globals.styled) file.set('hasStyled', true);
       },
       Program: {
-        enter(path, { file }) {
+        enter: (path, {file}) => {
           file.set('hasStyled', false);
         },
+        exit: ({node, scope}, {file}) => {
+          if (!(file.get('hasStyled') && !scope.hasBinding('styled'))) return;
 
-        exit({ node, scope }, { file }) {
-          if (!(file.get('hasStyled') && !scope.hasBinding('styled'))) {
-            return;
-          }
+          const declaration = t.importDeclaration(
+            [t.importDefaultSpecifier(t.identifier('styled'))],
+            t.stringLiteral('styled-components')
+          );
 
-          const styledImportDeclaration = t.importDeclaration([
-            t.importDefaultSpecifier(t.identifier('styled')),
-          ], t.stringLiteral('styled-components'));
-
-          node.body.unshift(styledImportDeclaration);
-        },
-      },
-    },
+          node.body.unshift(declaration);
+        }
+      }
+    }
   };
-}
+};
